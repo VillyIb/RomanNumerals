4b313876b4f2d0e79a1c2c0ad85a17771ea75a71
Villy Jorgensen <villy4.ib.jorgensen@gmail.com>
2017-01-08 21:00:24 +0100

    Implemented, passing tests.

--- RomanNumerals.Test/UnitTest1.cs
+++ RomanNumerals.Test/UnitTest1.cs
@@ -10,17 +10,8 @@ namespace RomanNumerals.Test
     [TestClass]
     public class UnitTest1
     {
-        [ClassInitialize]
-        public static void SetupClass()
-        { }
 
-
-        [ClassCleanup]
-        public static void TeardownClass()
-        { }
-
-
-        private IRomanNumeralService RomaNumeralService {get;set;}
+        private IRomanNumeralService RomaNumeralService { get; set; }
 
 
         [Microsoft.VisualStudio.TestTools.UnitTesting.TestInitialize]
@@ -38,19 +29,115 @@ namespace RomanNumerals.Test
             SystemDateTime.Reset();
         }
 
-        
+
         [TestMethod]
-        public void Test_Parse()
+        public void Test_Parse_1()
         {
             Assert.AreEqual<int>(10, RomaNumeralService.Parse("X"));
         }
 
 
         [TestMethod]
-        public void Test_ToRomanNumber()
+        public void Test_ToRomanNumber_1()
         {
-            Assert.AreEqual<string>("X", RomaNumeralService.ToRomanNumber(10);
+            Assert.AreEqual<string>("X", RomaNumeralService.ToRomanNumber(10));
         }
+
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Test_ToRoman_zero()
+        {
+            RomaNumeralService.ToRomanNumber(0);
+        }
+
+
+        [TestMethod]
+        public void Test_ToRoman_zero_HasErrorMessage()
+        {
+            try
+            {
+                RomaNumeralService.ToRomanNumber(0);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsFalse(String.IsNullOrWhiteSpace(ex.Message));
+            }
+        }
+
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Test_ToRoman_4000()
+        {
+            RomaNumeralService.ToRomanNumber(4000);
+        }
+
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Test_Parse_Fail_4567()
+        {
+            Assert.AreEqual<int>(10, RomaNumeralService.Parse("MMMMDLXVII")); // 4567 outside valid range.
+        }
+
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Test_Parse_Fail_1999Perm()
+        {
+            Assert.AreEqual<int>(10, RomaNumeralService.Parse("XCMCMIX")); // 1999 permutated
+        }
+
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Test_Parse_Fail_2444Perm()
+        {
+            Assert.AreEqual<int>(10, RomaNumeralService.Parse("XLMMCDIV")); // 2444 permutated
+        }
+
+
+        [TestMethod]
+        public void Test_Parse_HasErrorMessage_2444Perm()
+        {
+            try
+            {
+                RomaNumeralService.Parse("XLMMCDIV"); // 2444 permutated
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsFalse(String.IsNullOrWhiteSpace(ex.Message));
+            }
+        }
+
+
+        [TestMethod]
+        public void Test_ToRomanNumber_1897()
+        {
+            Assert.AreEqual<string>("MDCCCXCVII", RomaNumeralService.ToRomanNumber(1897));
+        }
+
+
+        [TestMethod]
+        public void Test_ToRomanNumber_acceptance()
+        {
+            Assert.AreEqual<string>("MCMXCIX", RomaNumeralService.ToRomanNumber(1999));
+            Assert.AreEqual<string>("MMCDXLIV", RomaNumeralService.ToRomanNumber(2444));
+            Assert.AreEqual<string>("XC", RomaNumeralService.ToRomanNumber(90));
+        }
+
+
+        [TestMethod]
+        public void Test_Parse_Acceptance()
+        {
+            Assert.AreEqual<int>(1999, RomaNumeralService.Parse("MCMXCIX"));
+            Assert.AreEqual<int>(2444, RomaNumeralService.Parse("MMCDXLIV"));
+            Assert.AreEqual<int>(90, RomaNumeralService.Parse("XC"));
+        }
+
 
     }
 }
--- RomanNumerals/RomanNumeralsService.cs
+++ RomanNumerals/RomanNumeralsService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 using RomanNumeralContract;
 
@@ -9,12 +10,43 @@ namespace RomanNumerals
 
         public int Parse(string romanNumber)
         {
-            throw new NotImplementedException();
+            var result = 0;
+
+            var current = romanNumber.Trim();
+            foreach (var converter in RomanDecade.RomanDecadeList)
+            {
+                var singleDigitValue = converter.GetValueOfFirstRomanDigit(current);
+                var sizeOfRomanDigit = converter.GetRomanDigit(singleDigitValue).Length;
+                var remaining = current.Substring(sizeOfRomanDigit);
+                result += singleDigitValue;
+                current = remaining;
+            }
+
+            var t1 = ToRomanNumber(result);
+            if (current.Length > 0) throw new ArgumentOutOfRangeException(nameof(romanNumber), String.Format("Parse '{0}', stopped parsing after '{1}'", romanNumber, t1));
+
+            if (result == 0) throw new ArgumentOutOfRangeException(nameof(romanNumber), String.Format("Parse '{0}', result 0 is not valid", romanNumber));
+            
+            if (!(romanNumber.Trim().Equals(t1, StringComparison.OrdinalIgnoreCase))) throw new ArgumentOutOfRangeException(nameof(romanNumber), String.Format("Parse '{0}', not matching '{1}", romanNumber, t1));
+
+            return result;
         }
+
 
         public string ToRomanNumber(int value)
         {
-            throw new NotImplementedException();
+            if (value < 1) throw new ArgumentOutOfRangeException(nameof(value), "min 1");
+            if (value > 3999) throw new ArgumentOutOfRangeException(nameof(value), "max 3999");
+
+            var result = new StringBuilder();
+
+            foreach (var converter in RomanDecade.RomanDecadeList)
+            {
+                var singelDigitValue = converter.GetSingleDigitValue(value);
+                result.Append(converter.GetRomanDigit(singelDigitValue));
+            }
+
+            return result.ToString();
         }
     }
 }
